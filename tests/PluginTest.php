ec517a70ba849d6e5f6bf4dfac3d200eae374be3
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2017-04-19 21:40:45 +0200

    Add tests

---
+++ tests/PluginTest.php
@@ -0,0 +1,13 @@
+<?php namespace VojtaSvoboda\BugSnag\Tests;
+
+use PluginTestCase;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class PluginTest extends PluginTestCase
+{
+    public function testComposerWasFine()
+    {
+        $this->assertTrue(class_exists(Bugsnag\Client::class));
+    }
+}
