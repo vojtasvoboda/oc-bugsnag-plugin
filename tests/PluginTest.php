ce96f08fe66002af85ba3b563cc70daf78576a39
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2018-04-13 22:17:43 +0200

    Fix tests

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,6 +1,5 @@
 <?php namespace VojtaSvoboda\BugSnag\Tests;
 
-use Bugsnag_Client;
 use PluginTestCase;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -9,6 +8,6 @@ class PluginTest extends PluginTestCase
 {
     public function testComposerWasFine()
     {
-        $this->assertTrue(class_exists(Bugsnag_Client::class));
+        $this->assertTrue(class_exists(\Bugsnag\Client::class));
     }
 }
